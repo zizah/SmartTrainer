8f1407a582bf7b8f3874834807e9bc048e58225b
Angele <giraudae@hotmail.fr>
2016-01-20 08:37:39 +0100

    Authentification

--- main.py
+++ main.py
@@ -16,10 +16,27 @@
 #
 from google.appengine.api import users
 import webapp2
+from model.Message import Message
 import globales
+from Admin.welcome_message import AddMessage
+from memcache.memcache_client import MemCacheClient
+from google.appengine.api import memcache
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        msg = memcache.get('welcome_msg')
+        if msg:
+            print "exists bingo"
+        else:
+            print "not exist"
+            welcome = Message()
+            welcome_msg = welcome.get_by_id(5629499534213120)
+            msg = str(welcome_msg.msg)
+            memcache.add(key="welcome_msg", value=str(msg), time=3600)
+          # self.response.write(globales.index.render(msg=msg))
+        self.response.write(globales.add_training.render())
+
         # Checks for active Google account session
         user = users.get_current_user()
         if user:
@@ -27,8 +44,10 @@ class MainHandler(webapp2.RequestHandler):
         else:
            self.response.write(globales.SEARCH.render(user=None))
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/Admin/welcome_message', AddMessage)
 ], debug=True)
 
 
