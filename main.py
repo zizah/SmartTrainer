f463af4d0bf16350d311211f9e2996bf9e547af7
Angele <giraudae@hotmail.fr>
2016-01-20 08:35:31 +0100

    Merge branch 'master' of C:\Users\Angèle\Documents\Python Cloud\SmartTrainer with conflicts.

--- main.py
+++ main.py
@@ -14,30 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
 import webapp2
-from model.Message import Message
 import globales
-from Admin.welcome_message import AddMessage
-from memcache.memcache_client import MemCacheClient
-from google.appengine.api import memcache
-
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        # msg = memcache.get('welcome_msg')
-        # if msg:
-        #     print "exists bingo"
-        # else:
-        #     print "not exist"
-        #     welcome = Message()
-        #     welcome_msg = welcome.get_by_id(5629499534213120)
-        #     msg = str(welcome_msg.msg)
-        #     memcache.add(key="welcome_msg", value=str(msg), time=3600)
-        #
-        # self.response.write(globales.index.render(msg=msg))
-        self.response.write(globales.add_training.render())
+        # Checks for active Google account session
+        user = users.get_current_user()
+        if user:
+            self.response.write(globales.SEARCH.render(user=user))
+        else:
+           self.response.write(globales.SEARCH.render(user=None))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/Admin/welcome_message', AddMessage)
+    ('/', MainHandler)
 ], debug=True)
+
+
+
+
