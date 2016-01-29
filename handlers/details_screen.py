98291fdf8c4fb464cfaf6fe3afb348eb1d61d4bd
melvin <melvinmasdieu@gmail.com>
2016-01-30 00:56:20 +0100

    exercice 5

--- handlers/details_screen.py
+++ handlers/details_screen.py
@@ -14,7 +14,7 @@ class Details(webapp2.RequestHandler):
         if user:
             url = users.create_logout_url('/')
             if data:
-                # data = json.load(data)
+                data = json.load(data)
                 plan = PlanEntrainement.get_by_id(data.get('id'))
                 exercices = Exercice.get_exercices_from_ancestor(plan)
                 self.response.write(globales.result_detail.render(user=user, url=url, plan=plan.title,exercices=exercices))
